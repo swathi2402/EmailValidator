8822bd75db722eca470f3dbcc0731433eb218b67
Swathi <swathi.h@perfios.com>
2021-10-07 19:57:12 +0530

    MODIFIED: RegEx to validate symbol and  domainname in a email

--- EmailValidator.js
+++ EmailValidator.js
@@ -2,7 +2,17 @@ let arguments = process.argv;
 arguments.splice(0,2);
 
 // UC 1
-let emailRegex = RegExp('^[a-z]+');
-arguments.forEach(element => {
-    console.log(element + " :", emailRegex.test(element));
-});
+{
+    let emailRegex = RegExp('^[a-z]+');
+    arguments.forEach(element => {
+        console.log(element + " :", emailRegex.test(element));
+    });
+}
+
+// UC 2
+{
+    let emailRegex = RegExp('^[a-z]+@[a-z]+');
+    arguments.forEach(element => {
+        console.log(element + " :", emailRegex.test(element));
+    });
+}
