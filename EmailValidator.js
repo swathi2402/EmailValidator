22d3a644f26e6d31dba5c10b6522595d7ec38797
Swathi <swathi.h@perfios.com>
2021-10-07 20:01:23 +0530

    MODIFIED: Handling optional part of the email

--- EmailValidator.js
+++ EmailValidator.js
@@ -24,3 +24,19 @@ arguments.splice(0,2);
         console.log(element + " :", emailRegex.test(element));
     });
 }
+
+// UC 4
+{
+    let emailRegex = RegExp('^[a-z]+([\.\+\_\-][a-z]+)?@[a-z]+.[a-z]+');
+    arguments.forEach(element => {
+        console.log(element + " :", emailRegex.test(element));
+    });
+}
+
+// UC 5
+{
+    let emailRegex = RegExp('^[a-z]+([\.\+\_\-][a-z]+)?@[a-z]+.[a-z]+(\.[a-z]{2})?$');
+    arguments.forEach(element => {
+        console.log(element + " :", emailRegex.test(element));
+    });
+}
